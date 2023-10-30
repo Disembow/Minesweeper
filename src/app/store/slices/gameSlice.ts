6d7a94afb512aca06e9feb5f29e70b30bdb432a5
Disembow <e_naletov@mail.ru>
2023-10-30 10:19:03 +0100

    refactor: add initial redux game state

--- src/app/store/slices/gameSlice.ts
+++ src/app/store/slices/gameSlice.ts
@@ -1,8 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { GameModes } from '../../db/db';
 
-interface IInitialState {}
+type GameType = {
+  isMine: boolean;
+  isOpen: boolean;
+  minesAround: number;
+  cellX: number;
+  cellY: number;
+  flag: boolean;
+};
 
-const initialState: IInitialState = {};
+interface IInitialState {
+  gameMode: GameModes;
+  game: GameType[][] | null;
+  currentMines: number | null;
+  openedCells: number | null;
+  timer: number;
+  interval: ReturnType<typeof setInterval> | null;
+  isMouseDown: boolean;
+  isGameRuns: boolean;
+}
+
+const initialState: IInitialState = {
+  gameMode: GameModes.BEGINNER,
+  game: null,
+  currentMines: null,
+  openedCells: null,
+  timer: 0,
+  interval: null,
+  isMouseDown: false,
+  isGameRuns: false,
+};
 
 const gameSlice = createSlice({
   name: 'game',

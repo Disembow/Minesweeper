c6246035246824c43ca55d9fc72d9191e75a6b3e
Disembow <e_naletov@mail.ru>
2023-12-13 17:33:22 +0100

    fix: cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:6606',
+    baseUrl: 'http://localhost:6600',
   },
   component: {
     devServer: {
